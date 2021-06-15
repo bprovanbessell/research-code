6af2781afe70decd150046cfa9c4c226f5d6f945
Ben <B.L.S.Provan-Bessell@student.tudelft.nl>
2021-06-15 17:00:54 +0200

    Update

---
+++ CNN-classifiers/single_label_classifier.py
@@ -0,0 +1,476 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+from torch.utils.data import Dataset
+from PIL import Image
+
+from sklearn.metrics import accuracy_score, f1_score, precision_score
+
+import json
+
+data_dir = "./data/hymenoptera_data"
+
+# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+model_name = "inception"
+
+# Number of classes in the dataset
+# total number of possible classes in the dilbert set:
+
+# Characters = Dilbert, Dogbert, Boss, Wolly, Alice, Catbert, Alice, Carol(Secretary), Tina, Ted (office guy) = 10, building
+# colours = blue, green, purple, yellow, pink, white = 5
+num_classes = 17
+
+# Batch size for training (change depending on how much memory you have)
+batch_size = 16
+
+# Number of epochs to train for
+num_epochs = 15
+
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+
+                np_labels = np.asarray(labels)
+
+                labels = torch.from_numpy(np.asarray(labels))
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels.type(torch.float))
+                        loss2 = criterion(aux_outputs, labels.type(torch.float))
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels.type(torch.float))
+
+                    # _, preds = torch.max(outputs, 1)
+                    threshold = 0.5
+                    preds = np.array(outputs > threshold, dtype=float)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                acc = accuracy_score(y_pred=preds, y_true=np_labels)
+                print(preds[0])
+                print(np_labels[0])
+                print("accuracy: ", acc)
+                f1 = f1_score(y_pred=preds, y_true=np_labels, average="samples")
+                print("f1:", f1)
+                precision = precision_score(y_pred=preds, y_true=np_labels, average="samples")
+                print("precision:", precision)
+
+
+                preds = torch.from_numpy(np.asarray(preds)).type(torch.float)
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels)
+
+                print(running_corrects)
+
+
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Sequential(
+            nn.Linear(num_ftrs, num_classes),
+            nn.Sigmoid())
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Sequential(nn.Linear(num_ftrs,num_classes), nn.Sigmoid())
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    return model_ft, input_size
+
+
+
+class inception_classifier(nn.Module):
+
+    def __init__(self, n_classes):
+        super(inception_classifier, self).__init__()
+
+        self.num_classes = n_classes
+
+        model = models.inception_v3()
+        url = 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth'
+        model.load_state_dict(model_zoo.load_url(url))
+        for param in model.parameters():
+            param.requires_grad = False
+        print('Load pretrained model from ', url)
+        # print(model)
+
+        self.define_module(model)
+        # self.init_trainable_weights()
+
+    def define_module(self, model):
+        self.Conv2d_1a_3x3 = model.Conv2d_1a_3x3
+        self.Conv2d_2a_3x3 = model.Conv2d_2a_3x3
+        self.Conv2d_2b_3x3 = model.Conv2d_2b_3x3
+        self.Conv2d_3b_1x1 = model.Conv2d_3b_1x1
+        self.Conv2d_4a_3x3 = model.Conv2d_4a_3x3
+        self.Mixed_5b = model.Mixed_5b
+        self.Mixed_5c = model.Mixed_5c
+        self.Mixed_5d = model.Mixed_5d
+        self.Mixed_6a = model.Mixed_6a
+        self.Mixed_6b = model.Mixed_6b
+        self.Mixed_6c = model.Mixed_6c
+        self.Mixed_6d = model.Mixed_6d
+        self.Mixed_6e = model.Mixed_6e
+        self.Mixed_7a = model.Mixed_7a
+        self.Mixed_7b = model.Mixed_7b
+        self.Mixed_7c = model.Mixed_7c
+
+        # self.emb_features = conv1x1(768, self.nef)
+        # self.emb_cnn_code = nn.Linear(2048, self.nef)
+        self.fc = nn.Linear(2048, num_classes)
+
+        self.sigmoid = nn.Sigmoid()
+
+    # def init_trainable_weights(self):
+    #     initrange = 0.1
+    #     # self.emb_features.weight.data.uniform_(-initrange, initrange)
+    #     # self.emb_cnn_code.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, x):
+        features = None
+        # --> fixed-size input: batch x 3 x 299 x 299
+        x = nn.functional.interpolate(x,size=(299, 299), mode='bilinear', align_corners=False)
+        # 299 x 299 x 3
+        x = self.Conv2d_1a_3x3(x)
+        # 149 x 149 x 32
+        x = self.Conv2d_2a_3x3(x)
+        # 147 x 147 x 32
+        x = self.Conv2d_2b_3x3(x)
+        # 147 x 147 x 64
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # 73 x 73 x 64
+        x = self.Conv2d_3b_1x1(x)
+        # 73 x 73 x 80
+        x = self.Conv2d_4a_3x3(x)
+        # 71 x 71 x 192
+
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # 35 x 35 x 192
+        x = self.Mixed_5b(x)
+        # 35 x 35 x 256
+        x = self.Mixed_5c(x)
+        # 35 x 35 x 288
+        x = self.Mixed_5d(x)
+        # 35 x 35 x 288
+
+        x = self.Mixed_6a(x)
+        # 17 x 17 x 768
+        x = self.Mixed_6b(x)
+        # 17 x 17 x 768
+        x = self.Mixed_6c(x)
+        # 17 x 17 x 768
+        x = self.Mixed_6d(x)
+        # 17 x 17 x 768
+        x = self.Mixed_6e(x)
+        # 17 x 17 x 768
+
+        # image region features
+        features = x
+        # 17 x 17 x 768
+
+        x = self.Mixed_7a(x)
+        # 8 x 8 x 1280
+        x = self.Mixed_7b(x)
+        # 8 x 8 x 2048
+        x = self.Mixed_7c(x)
+        # 8 x 8 x 2048
+        x = F.avg_pool2d(x, kernel_size=8)
+        # 1 x 1 x 2048
+        # x = F.dropout(x, training=self.training)
+        # 1 x 1 x 2048
+        x = x.view(x.size(0), -1)
+        # 2048
+
+        # global image features
+        # cnn_code = self.emb_cnn_code(x)
+        # # 512
+        # if features is not None:
+        #     features = self.emb_features(features)
+        # return features, cnn_code
+
+#         do the classification
+        final_layer = self.fc(x)
+
+        output = self.sigmoid(final_layer)
+
+        return output
+
+    # image_transform = transforms.Compose([
+    #     transforms.Resize(int(imsize * 76 / 64)),
+    #     transforms.RandomCrop(imsize),
+    #     transforms.RandomHorizontalFlip()])
+
+class LabelsDataset(Dataset):
+    def __init__(self, img_dir, labels_json, split='train',
+                 base_size=64,
+                 transform=None, target_transform=None):
+        self.transform = transform
+        self.norm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self.target_transform = target_transform
+        # self.embeddings_num = cfg.TEXT.CAPTIONS_PER_IMAGE
+
+        # self.imsize = []
+        # for i in range(cfg.TREE.BRANCH_NUM):
+        #     self.imsize.append(base_size)
+        #     base_size = base_size * 2
+
+        self.data = []
+        self.img_dir = img_dir
+
+        with open(labels_json) as labels_file:
+            self.labels_dict = json.load(labels_file)
+            self.img_names = list(self.labels_dict.keys())
+
+    def __len__(self):
+        return len(self.labels_dict)
+
+    def __getitem__(self, idx):
+
+        key = self.img_names[idx]
+        img_path = os.path.join(self.img_dir, key)
+        image = self.get_imgs(img_path, 299, self.transform, normalize=self.norm)
+        label = self.labels_dict[key]
+        # if self.transform:
+        #     image = self.transform(image)
+        # if self.target_transform:
+        #     label = self.target_transform(label)
+        return image, np.asarray(label, dtype=float)
+
+
+    def get_imgs(self, img_path, imsize, bbox=None,
+                 transform=None, normalize=None):
+        img = Image.open(img_path).convert('RGB')
+        width, height = img.size
+
+        # print(width)
+
+        if transform is not None:
+            img = transform(img)
+
+        re_img = transforms.Resize(imsize)(img)
+        # re_img = img
+
+        # print(re_img.size)
+        norm_image = normalize(re_img)
+
+        return norm_image
+
+        # ret = []
+        # if cfg.GAN.B_DCGAN:
+        #     ret = [normalize(img)]
+        # else:
+        #     for i in range(cfg.TREE.BRANCH_NUM):
+        #         # print(imsize[i])
+        #         if i < (cfg.TREE.BRANCH_NUM - 1):
+        #             re_img = transforms.Resize(imsize[i])(img)
+        #         else:
+        #             re_img = img
+        #         ret.append(normalize(re_img))
+
+
+if __name__ == "__main__":
+
+    img_dir = "../data/dilbert/resized_256_07-11"
+    train_json = "../data/dilbert/annotated-jsons/train_labels.json"
+    test_json = "../data/dilbert/annotated-jsons/test_labels.json"
+
+    imsize = 299
+    image_transform = transforms.Compose([
+        transforms.Resize(int(imsize * 76 / 64)),
+        transforms.RandomCrop(imsize),
+        transforms.RandomHorizontalFlip()])
+
+    train_dataset = LabelsDataset(img_dir, train_json, transform=image_transform)
+    test_dataset = LabelsDataset(img_dir, test_json, transform=image_transform)
+
+    dataloaders_dict = {
+        'train': torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4),
+        'val': torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)}
+
+    # Initialize the model for this run
+    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+    # Print the model we just instantiated
+    print(model_ft)
+
+    model_ft = model_ft.to(device)
+
+    # Gather the parameters to be optimized/updated in this run. If we are
+    #  finetuning we will be updating all parameters. However, if we are
+    #  doing feature extract method, we will only update the parameters
+    #  that we have just initialized, i.e. the parameters with requires_grad
+    #  is True.
+    params_to_update = model_ft.parameters()
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t",name)
+    else:
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                print("\t",name)
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCELoss()
+
+
+    # Train and evaluate
+    model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs, is_inception=(model_name=="inception"))
